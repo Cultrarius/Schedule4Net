8ff4425a25c2459a2e390c4b7bc992b2caa1b41b
Michael <Galetzka.Michael@gmail.com>
2013-02-13 01:54:03 +0100

    added constraint implementations

---
+++ Schedule4Net/Constraint/Impl/DebugTestConstraint.cs
@@ -0,0 +1,51 @@
+﻿using Schedule4Net.Core;
+
+namespace Schedule4Net.Constraint.Impl
+{
+    class DebugTestConstraint : ItemPairConstraint
+    {
+        ConstraintDecision ItemPairConstraint.Check(ScheduledItem item1, ScheduledItem item2)
+        {
+            bool isFulfilled = true;
+            int overlappedValue = 0;
+            if (item1.ItemToSchedule.Id % 10 == item2.ItemToSchedule.Id % 10)
+            {
+                foreach (Lane lane1 in item1.ItemToSchedule.Lanes)
+                {
+                    foreach (Lane lane2 in item2.ItemToSchedule.Lanes)
+                    {
+                        int overlapping = ScheduleUtil.GetOverlappingValue(item1.Start, item1.GetEnd(lane1), item2.Start,
+                                item2.GetEnd(lane2));
+                        if (overlapping <= 0) continue;
+                        isFulfilled = false;
+                        overlappedValue += overlapping;
+                    }
+                }
+            }
+            return new ConstraintDecision(true, isFulfilled, overlappedValue);
+        }
+
+        bool ItemPairConstraint.NeedsChecking(ItemToSchedule item1, ItemToSchedule item2)
+        {
+            return item1.Id % 10 == item2.Id % 10;
+        }
+
+        ConstraintPrediction ItemPairConstraint.PredictDecision(ItemToSchedule movedItem, ItemToSchedule fixItem)
+        {
+            bool idsCollide = movedItem.Id % 10 == fixItem.Id % 10;
+            int overlappedValue = 0;
+            if (idsCollide)
+            {
+                foreach (Lane lane1 in movedItem.Lanes)
+                {
+                    foreach (Lane lane2 in fixItem.Lanes)
+                    {
+                        overlappedValue += ScheduleUtil.GetOverlappingValue(0, movedItem.GetDuration(lane1), 0, fixItem.GetDuration(lane2));
+                    }
+                }
+            }
+            return new ConstraintPrediction(ConstraintPrediction.Prediction.NoConflict, idsCollide ? ConstraintPrediction.Prediction.Conflict : ConstraintPrediction.Prediction.NoConflict,
+                    ConstraintPrediction.Prediction.NoConflict, overlappedValue);
+        }
+    }
+}
---
+++ Schedule4Net/Constraint/Impl/DependenciesConstraint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Schedule4Net.Core;
+
+namespace Schedule4Net.Constraint.Impl
+{
+    class DependenciesConstraint : ItemPairConstraint
+    {
+        public ConstraintDecision Check(ScheduledItem item1, ScheduledItem item2)
+        {
+            bool isFulfilled = true;
+            int overlappedValue = 0;
+            ItemToSchedule itemToSchedule1 = item1.ItemToSchedule;
+            ItemToSchedule itemToSchedule2 = item2.ItemToSchedule;
+            int distanceToEnd = 0;
+            if (itemToSchedule1.RequiredItems.Contains(itemToSchedule2))
+            {
+                distanceToEnd = ScheduleUtil.GetMinimumDistanceToEnd(item2, item1);
+            }
+            else if (itemToSchedule2.RequiredItems.Contains(itemToSchedule1))
+            {
+                distanceToEnd = ScheduleUtil.GetMinimumDistanceToEnd(item1, item2);
+            }
+            if (distanceToEnd < 0)
+            {
+                isFulfilled = false;
+                overlappedValue = Math.Max(item1.ItemToSchedule.DurationSummary, item2.ItemToSchedule.DurationSummary);
+            }
+            return new ConstraintDecision(true, isFulfilled, overlappedValue);
+        }
+
+        public bool NeedsChecking(ItemToSchedule item1, ItemToSchedule item2)
+        {
+            return item1.RequiredItems.Contains(item2) || item2.RequiredItems.Contains(item1);
+        }
+
+        public ConstraintPrediction PredictDecision(ItemToSchedule movedItem, ItemToSchedule fixItem)
+        {
+            int conflictValue = Math.Max(movedItem.DurationSummary, fixItem.DurationSummary);
+            if (fixItem.RequiredItems.Contains(movedItem))
+            {
+                return new ConstraintPrediction(ConstraintPrediction.Prediction.NoConflict, ConstraintPrediction.Prediction.Conflict, ConstraintPrediction.Prediction.Conflict, conflictValue);
+            }
+            if (movedItem.RequiredItems.Contains(fixItem))
+            {
+                return new ConstraintPrediction(ConstraintPrediction.Prediction.Conflict, ConstraintPrediction.Prediction.Conflict, ConstraintPrediction.Prediction.NoConflict, conflictValue);
+            }
+            return new ConstraintPrediction(ConstraintPrediction.Prediction.NoConflict, ConstraintPrediction.Prediction.NoConflict, ConstraintPrediction.Prediction.NoConflict, 0);
+        }
+    }
+}
