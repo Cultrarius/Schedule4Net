44ce11d164fe92766df07e637be816bb308e9743
Michael <Galetzka.Michael@gmail.com>
2013-02-15 01:11:10 +0100

    added abstract base classes for constraints

---
+++ Schedule4Net/Constraint/AbstractPairConstraint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Schedule4Net.Constraint
+{
+    public abstract class AbstractPairConstraint<T> : ItemPairConstraint where T : ItemToSchedule
+    {
+        public ConstraintDecision Check(ScheduledItem item1, ScheduledItem item2)
+        {
+            T it1 = item1 as T;
+            T it2 = item2 as T;
+            if (it1 == null || it2 == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return Check(it1, it2);
+        }
+
+        protected abstract ConstraintDecision Check(T item1, T item2);
+
+        public virtual bool NeedsChecking(ItemToSchedule item1, ItemToSchedule item2)
+        {
+            return (item1 is T) && (item2 is T);
+        }
+
+        public virtual ConstraintPrediction PredictDecision(ItemToSchedule movedItem, ItemToSchedule fixItem)
+        {
+            return new ConstraintPrediction(ConstraintPrediction.Prediction.Unknown, ConstraintPrediction.Prediction.Unknown, ConstraintPrediction.Prediction.Unknown, 0);
+        }
+    }
+}
---
+++ Schedule4Net/Constraint/AbstractSingleConstraint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Schedule4Net.Constraint
+{
+    public abstract class AbstractSingleConstraint<T> : SingleItemConstraint where T : ItemToSchedule
+    {
+        public ConstraintDecision Check(ScheduledItem item)
+        {
+            T it = item as T;
+            if (it == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return Check(it);
+        }
+
+        protected abstract ConstraintDecision Check(T item);
+    }
+}
